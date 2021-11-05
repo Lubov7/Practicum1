faf802ce7c96cce73a8dffcacb530eec7b511ec5
Любовь <miss.lubik@mail.ru>
2021-11-05 17:46:08 +0300

    Start

--- Dev01_Example/Program.cs
+++ Dev01_Example/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача
+// Cформировать случайным образом целочисленный массив A из натуральных двузначных чисел. 
+// Создать на его основе масив B, отбрасывая те, которые
+
+// 1. нарушают порядок возрастания
+// 2. больше среднего арифметического элементов A
+// 3. чётные
+
+
+
+// Задать массив 
+int[] array = new int[10];
+
+int index = 0;
+
+// Заполняет массив
+while (index < 10)
+{
+    array[index] = new Random().Next(10, 100);
+    index++;
+}
+
+index = 0;
+// Печатаем массив
+while (index <=9)
+{
+    int val = array[index];
+    Console.WriteLine(val);
+    index++;
+}
+
+Console.WriteLine();
+index = 1;
+// Исключение элементов которые нарушают порядок возрастания
+int currentElement = array[0];
+Console.WriteLine(currentElement);
+while(index <= 9)
+{
+    if(array[index] > currentElement)
+    {
+        Console.WriteLine(array[index]);
+        currentElement=array[index];
+    }
+    index++;
+}
+
